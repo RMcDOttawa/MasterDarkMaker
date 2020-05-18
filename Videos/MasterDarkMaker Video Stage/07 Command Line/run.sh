47fdf9cb539cad1fb5f7a078ddabe9637d09be58
Richard McDonald <richard@themcdonalds.net>
2020-05-18 14:16:45 -0400

    Convert exposure and temperature clustering to use a local implementation of mean_shift instead of sklearn, to avoid problems with pyinstaller.

---
+++ Videos/MasterDarkMaker Video Stage/07 Command Line/run.sh
@@ -0,0 +1,8 @@
+./mclMBM  \
+	-s 2 \
+	-gs \
+	\
+	-od /Users/richard/DropBox/dropbox/EWHO/Application\ Development/MasterBiasMaker/Videos/MasterBiasMaker\ Video\ Stage/cmdLineOutput \
+	\
+	/Users/richard/DropBox/dropbox/EWHO/Application\ Development/MasterBiasMaker/Videos/MasterBiasMaker\ Video\ Stage/02\ Raw\ Bias\ Files/*BIAS*.fit
+	
